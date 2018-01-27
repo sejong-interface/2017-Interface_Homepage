268ffda250ae8d0103cc5dbb073d99e05fcb630f
2kyung19 <32586712+2kyung19@users.noreply.github.com>
2018-01-27 18:49:00 +0900

    28일 과제 (페이지 소스뜯기)

---
+++ study/2kyung19/인터페이스 홈 과제1/js/app.js
@@ -0,0 +1,37 @@
+function number_format(data)
+{
+
+    var tmp = '';
+    var number = '';
+    var cutlen = 3;
+    var comma = ',';
+    var i;
+
+    var sign = data.match(/^[\+\-]/);
+    if(sign) {
+        data = data.replace(/^[\+\-]/, "");
+    }
+
+    len = data.length;
+    mod = (len % cutlen);
+    k = cutlen - mod;
+    for (i=0; i<data.length; i++)
+    {
+        number = number + data.charAt(i);
+
+        if (i < data.length - 1)
+        {
+            k++;
+            if ((k % cutlen) == 0)
+            {
+                number = number + comma;
+                k = 0;
+            }
+        }
+    }
+
+    if(sign != null)
+        number = sign+number;
+
+    return number;
+}
